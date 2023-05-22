37ca1519fe77c96391e761f7f454e314dc7306dd
leduyphong123 <leduyphongck789@gmail.com>
2023-05-22 15:47:08 +0700

    finnish

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main{
+    public static void main(String[] args){
+        String text="Able was I ere I saw Elba";
+        String newText=text.toLowerCase();
+        int index=newText.length();
+        boolean result=true;
+        Stack stack= new Stack();
+        Queue queue = new LinkedList();
+        for (int i=0;i<index;i++){
+            stack.push(newText.charAt(i));
+            queue.add(newText.charAt(i));
+        }
+        for (int i=0;i<index;i++){
+            if(!queue.poll().equals(stack.get(index-i-1))){
+                result=false;
+                break;
+            }
+        }
+        if(result){
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("no  Palindrome");
+
+        }
+    }
+}
